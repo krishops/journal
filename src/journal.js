3975d1af7c0d67744cd842aff26655acf1a7dbcd
Kris Hopper <hopperdavis@gmail.com>
2021-06-21 17:55:36 -0700

    add vowel count functionality

--- src/journal.js
+++ src/journal.js
@@ -6,3 +6,49 @@ export default function JournalEntry(journalTitle, journalEntry) {
 JournalEntry.prototype.returnEntry = function() {
   return "Entry Recorded";
 };
+
+JournalEntry.prototype.returnWordCount = function(entry) {
+  let wordCount = entry.split(" ");
+  console.log(wordCount);
+  return wordCount.length;
+};
+
+JournalEntry.prototype.countCharacters = function(entry) {
+  let entryCharacters = entry.split("");
+  console.log(entryCharacters);
+  let vowelCount = 0;
+  let consonantCount = 0;
+
+  const vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'];
+  const consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+  
+  entryCharacters.forEach(function(letter)  {
+    for (let i = 0; i < consonants.length; i +=1) {
+      if (letter === consonants[i])  {
+        consonantCount++;
+      }
+    }
+    for (let i = 0; i < vowels.length; i +=1) {
+      if (letter === vowels[i])  {
+        vowelCount++;
+      } 
+    }
+  });
+  return  {
+    vowels: vowelCount,
+    consonants: consonantCount,
+  }  
+};
+  // for (let i = 0; i < vowels.length; i +=1){
+  //   let count = 0;
+  //   if (vowels[i] === "a") {
+  //     return count += 1 ;
+  // };
+
+JournalEntry.prototype.getTeaser = function(entry)  {
+  let sentences = entry.split(".");
+    return sentences[0]
+  };
+
+  
+
--- src/main.js
+++ src/main.js
@@ -8,10 +8,17 @@ $(document).ready(function() {
   $('#my-journal').submit(function(event) {
     event.preventDefault();
     const inputtedJournalTitle = $('input#journal-title').val();
-    const inputtedJournalEntry = $('input#ljournal-entry').val();
+    const inputtedJournalEntry = $('input#journal-entry').val();
     let journalEntry = new JournalEntry(inputtedJournalTitle, inputtedJournalEntry);
     console.log(journalEntry);
     const recordedEntry = journalEntry.returnEntry();
+    const wordCount = journalEntry.returnWordCount(inputtedJournalEntry);
+    const returnCharacters = journalEntry.countCharacters(inputtedJournalEntry);
+    const firstSentence = journalEntry.getTeaser(inputtedJournalEntry);
     $('#recorded-entry').append("<p>" + recordedEntry + "</p>");
+    $('#word-count').append("<p>Word Count: " + wordCount + "</p>");
+    $('#vowel-count').append("<p>Vowel Count: " + returnCharacters.vowels + "</p>");
+    $('#consonant-count').append("<p>Consonant Count: " + returnCharacters.consonants + "</p>");
+    $('#first-sentence').append("<p>Teaser: " + firstSentence + "</p>");
   });
 });
